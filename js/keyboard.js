b31ee677713f77b53eceacac80f9fed26f642651
Chahaaaaan <49513840+Chahaaaaan@users.noreply.github.com>
2019-12-05 18:53:50 +0900

    余分なコードを削除しました

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,8 +1,3 @@
-function keyboardTouchStart(position, element) {
-    window.keyboardTouchStartedAt = position;
-    window.keyboardTouchStartedOn = element;
-}
-
 function keyboardTouchMove(position) {
     window.keyboardTouchMoveAt = position;
 }
