33192ca7c655d9fdddfbafa657a6d1eae063714a
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-12-08 13:50:25 +0900

    keyboardSlideUp 関数を追加

--- js/keyboard.js
+++ js/keyboard.js
@@ -6,3 +6,14 @@ function keyboardTouchStart(position, element) {
 function keyboardTouchMove(position) {
     window.keyboardTouchMoveAt = position;
 }
+
+function keyboardSlideUp(column) {
+    $('.keyboard-key')
+        .filter(function() {
+            return parseInt($(this).css('grid-column-start')) === column;
+        })
+        .css('grid-row-start', function(index, value) {
+            const current = parseInt(value);
+            return current === 1 ? window.config.keyboard.rows : current - 1;
+        });
+}
