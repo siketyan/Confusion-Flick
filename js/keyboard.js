b15089bce9f5d780d53bb7caf21e559244b4293f
RAINeko <reirain256@gmail.com>
2019-12-09 17:45:07 +0900

    (Close #36)右スライドの実装

--- js/keyboard.js
+++ js/keyboard.js
@@ -6,3 +6,20 @@ function keyboardTouchStart(position, element) {
 function keyboardTouchMove(position) {
     window.keyboardTouchMoveAt = position;
 }
+
+function keyboardSlideRight(row){
+
+	$('.keyboard-key')
+	.filter(function(){
+
+		return parseInt($(this).css('grid-row-start'))==row;
+
+	})
+
+	.css('grid-column-start',function(index,value){
+		var current = parseInt(value);
+		return current == window.config.keyboard.columns ? 1 : current+1;
+
+	})
+
+}
