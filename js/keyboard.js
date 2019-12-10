d17913a564290e00a331d673f4d77887f59916b8
natu104 <narutaka104763@yahoo.co.jp>
2019-12-10 17:02:31 +0900

    下スライド実装

--- js/keyboard.js
+++ js/keyboard.js
@@ -7,6 +7,10 @@ function keyboardTouchMove(position) {
     window.keyboardTouchMoveAt = position;
 }
 
+function keyboardTouchEnd(){
+    
+}
+
 function keyboardSlideUp(column) {
     $('.keyboard-key')
         .filter(function() {
@@ -15,5 +19,16 @@ function keyboardSlideUp(column) {
         .css('grid-row-start', function(index, value) {
             const current = parseInt(value);
             return current === 1 ? window.config.keyboard.rows : current - 1;
+        });
+}
+
+function keyboardSlideDown(column) {
+    $('.keyboard-key')
+        .filter(function() {
+            return parseInt($(this).css('grid-column-start')) === column;
+        })
+        .css('grid-row-start', function(index, value) {
+            const current = parseInt(value);
+            return current === window.config.keyboard.rows ? 1 : current - 1;
         });
 }
