89600bc84ba45caead648823698c34227c978b29
chahaaaaan <chahaaaaan@gmail.com>
2019-12-12 17:49:49 +0900

    フリックした方向を取得する関数を作成しました

--- js/keyboard.js
+++ js/keyboard.js
@@ -34,3 +34,31 @@ function keyboardSlideUp(column) {
             return current === 1 ? window.config.keyboard.rows : current - 1;
         });
 }
+function keyboardResolveFlick(){
+    var RelativeX = window.keyboardTouchStartedAt.x - window.keyboardTouchMoveAt.x;
+    var RelativeY = window.keyboardTouchStartedAt.y - window.keyboardTouchMoveAt.y;
+    var isX = Math.abs(RelativeX) > Math.abs(RelativeY) ? true : false;
+    var distance = Math.abs(RelativeX) > Math.abs(RelativeY) ? RelativeX : RelativeY;
+    if (RelativeX === RelativeY) {
+        return;
+    }
+    if(window.config.keyboard.threshold > distance){
+        return 0;
+    }
+    //左スライドを呼ぶ
+    if (isX && distance > 0) {
+        return 1;
+    }
+    //上スライド
+    else if (!isX && distance > 0) {
+        return 2;
+    }
+    //右スライド
+    else if (isX && distance < 0) {
+        return 3;
+    }
+    //下スライド
+    else {
+        return 4;
+    }
+}
