e21d7785018b2bee5695b4caac3ab33e6d7a97e1
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-12-12 15:09:12 +0900

    キーボードを左方向にスライドする関数
    キーボードを左方向にスライドする関数を作成しました
    
    消えてたkeyboardTouchStartを追加しました
    Update keyboard.js
    Update keyboard.js
    Update keyboard.js
    Update keyboard.js
    Update keyboard.js

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,4 +1,14 @@
 function keyboardTouchStart(position, element) {
     window.keyboardTouchStartedAt=position;
     window.keyboardTouchStartedOn=element;
+} 
+function keyboardSlideLeft(row){
+     $('.keyboard-key')
+     .filter(function() {
+         return parseInt($(this).css('grid-row-start')) === row;
+     })
+     .css('grid-column-start', function(index, value) {
+          const current = parseInt(value);
+          return current === 1 ? window.config.keyboard.columns : current - 1;
+      });
 }
