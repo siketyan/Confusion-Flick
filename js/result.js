37bd2e557df3f071d32aa175529c638d78231219
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-12-14 23:53:37 +0900

    ツイートボタンを追加

--- js/result.js
+++ js/result.js
@@ -3,3 +3,20 @@ function resultShow() {
     $('#result-characters').text(window.gameCharacterCount);
     $('#result-misses').text(window.gameMissCount);
 }
+
+function resultTweet() {
+    const counts = [
+        window.gameSentenceCount,
+        window.gameCharacterCount,
+        window.gameMissCount,
+    ];
+
+    const body = `Confusion Flick で ${counts[0]} つの問題 (${counts[1]} 文字) を ${counts[2]} ミスで入力しました！`;
+    const suffix = '#Conflick https://conflick.now.sh/';
+    const text = encodeURIComponent([body, suffix].join('\n'));
+
+    window.open(
+        'https://twitter.com/intent/tweet?text=' + text,
+        '_blank'
+    );
+}
