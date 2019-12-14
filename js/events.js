f41f984c3fe80ce1176487fe8f43c6a18194999c
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-12-14 23:10:31 +0900

    セミコロンをつける

--- js/events.js
+++ js/events.js
@@ -4,22 +4,22 @@ function registerEvents() {
         const callback = page === 'game' ? gameStart : null;
 
         pageLoad(page, callback);
-    })
+    });
 
     $(".keyboard-key").on('touchstart', function (event) {
         const position = {
             x: event.changedTouches[0].pageX,
             y: event.changedTouches[0].pageY,
-        }
+        };
         keyboardTouchStart(position, this);
     })
-        .on('touchend', keyboardTouchEnd)
+        .on('touchend', keyboardTouchEnd);
 
     $(window).on('touchmove', function (event) {
         const position = {
             x: event.changedTouches[0].pageX,
             y: event.changedTouches[0].pageY,
-        }
+        };
         keyboardTouchMove(position);
     })
 }
